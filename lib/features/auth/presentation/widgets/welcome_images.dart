7fd2faa01c055c68d9484d0cab753a032529832a
don-okal <ahmedokal6996@gmail.com>
2024-08-04 10:20:39 +0300

    image size

--- lib/features/auth/presentation/widgets/welcome_images.dart
+++ lib/features/auth/presentation/widgets/welcome_images.dart
@@ -17,7 +17,7 @@ class WelcomeImages extends StatelessWidget {
           children: [
             Positioned(
               left: 160.w,
-              top: 80.h,
+              top: 70.h,
               child: Image.asset(
                 AssetsData.boy,
                 width: 200.w,
